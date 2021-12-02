4b6da60d3ae4673a2d665f98db4e443a0c21968d
Tenzin Jorden <tenden96@gmail.com>
2021-12-02 13:47:00 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector("#main");
+
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML="Tenzin is the champion";
+document.body.append(newHeader);
